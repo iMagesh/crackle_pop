e1cea91dd2ff64a7fcdd9488b01b9528362800aa
Magesh <pingmagesh@gmail.com>
2026-08-28 15:06:39 +0530

    Added the core logic for cracke pop

---
+++ crackle_pop.rb
@@ -0,0 +1,23 @@
+# Core transformation logic
+def code_for(number)
+  return nil unless number.is_a?(Integer)
+  return 0 if number.zero?
+
+  case
+    when (number % 15).zero? then 'CracklePop'
+    when (number % 3).zero?  then 'Crackle'
+    when (number % 5).zero?  then 'Pop'
+  else number
+  end
+end
+
+# Main execution loop
+def crackle_pop(first = 1, last = 100)
+  return [] if first > last
+
+  first.upto(last).map do |number|
+    result = code_for(number)
+    puts result
+    result
+  end
+end
